7fee83eed8b5456b33d2fa974f5755c5853c1223
Juunnmmoo <juunnmmoo@gmail.com>
2026-09-22 16:13:37 +0900

    Lv 13. 채팅 요청 처리와 응답 구성

--- src/main/java/com/gameexpert/ws/dto/ChatResponse.java
+++ src/main/java/com/gameexpert/ws/dto/ChatResponse.java
@@ -6,6 +6,15 @@ import lombok.Getter;
 @Getter
 public class ChatResponse {
     // TODO Lv 13: API 명세에 맞게 응답 필드와 생성자를 완성합니다.
+    private final String type;
+    private final String sender;
+    private final String content;
+    private final String timestamp;
+
     public ChatResponse(String sender, String content, LocalDateTime timestamp) {
+        type = "chat";
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = String.valueOf(timestamp);
     }
 }
